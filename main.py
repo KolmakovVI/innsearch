07bddfe20d8698c0ca7b7fa1fa41941906283010
Kolmakov VI <kolmakowladimir@gmail.com>
2022-05-25 23:01:15 +0300

    try second metod -- insert INN into URL and parse HTML, bc i'm nuub(((((

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 
 import undetected_chromedriver as uc
 
+import time
+
 
 def inn_from_excel():
     inn = "507703578056"
@@ -9,15 +11,23 @@ def inn_from_excel():
 
 
 def search(inn):
+    # driver = uc.Chrome()
+    # driver.get("https://www.rusprofile.ru/?1")
+    # # os.system('pause') # for CAPCHA
+    # search_field = driver.find_element_by_name("query") #work
+    # #search_field = driver.find_element(by=By.NAME, value="query") # try to exiqute
+    # search_field.send_keys(inn)
+    # # how to push the button ????
+    # # how to find the button  ????
+    # button_field = driver.find_element_by_link_text('Найти')
+    # button_field.click()
+
+    #--------------------- second metod: insert INN to url-----------------------------
+    url_1 = "https://www.rusprofile.ru/search?query="
+    url_2 = "&type=ul"
+    full_url = url_1 + inn + url_2
     driver = uc.Chrome()
-    driver.get("https://www.rusprofile.ru/?1")
-    os.system('pause') # for CAPCHA
-    search_field = driver.find_element_by_name("query") #work
-    #search_field = driver.find_element(by=By.NAME, value="query") # try to exiqute
-    search_field.send_keys(inn)
-    # how to push the button ????
-    # how to find the button  ????
-    #button_field = driver.find_element_by_class_name('search-btn waves-effect waves-light')
+    driver.get(full_url)
 
 
 if __name__ == '__main__':
